4916ce0dfeda5198d3b2b672da3942e663eb8ed1
Mikhail Sotnikov <thehofch@gmail.com>
2018-12-03 19:55:24 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,6 +7,8 @@ set :user,            'lift'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
+set :rvm_type, :system
+
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
