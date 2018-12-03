2ad0c8973ce920b8d85a75fcae6c3847c8df5ce0
Mikhail Sotnikov <thehofch@gmail.com>
2018-12-03 20:11:47 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,7 +1,7 @@
 # Change these
 server '31.210.216.198', port: 22, roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:sotnikoff/pinger.git'
+set :repo_url,        'https://github.com/sotnikoff/pinger.git'
 set :application,     'pinger'
 set :user,            'lift'
 set :puma_threads,    [4, 16]
