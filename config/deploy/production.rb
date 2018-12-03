485ad6afcee9963669bf635fa5d0a40709b937d9
Mikhail Sotnikov <thehofch@gmail.com>
2018-12-03 19:59:34 +0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@ set :user,            'lift'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-set :rvm_type, :system
+set :rvm_custom_path, '/usr/share/rvm/bin/rvm'
 
 # Don't change these unless you know what you're doing
 set :pty,             true
